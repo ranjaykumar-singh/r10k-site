908cffe9c5691a9c1e36228d7a62cb8a0c23d8f8
Ranjay Singh <ranjaysingh.eidiko@gmail.com>
2018-03-16 02:16:33 +0530

    second commit

--- createfile/manifests/init.pp
+++ createfile/manifests/init.pp
@@ -2,8 +2,6 @@ class createfile{
 file { "/home/bandaru/Desktop/r10kdemo":
     ensure => 'file',
     mode => '777',
-    contents => 'This is for r10k',
-    owner => root,
-    group => root,
+    contents => 'This is for '
 }
 }
